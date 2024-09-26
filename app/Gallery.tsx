98d9ad8f299301e73804bcf9d022e5a688d8f84b
Tierra Williams <112350897+TierraCodes@users.noreply.github.com>
2024-09-26 02:03:32 -0400

    Added more functionality?

--- app/Gallery.tsx
+++ app/Gallery.tsx
@@ -1,10 +1,57 @@
+'use client'
+import {useState} from 'react';
+import Images from './page';
+import {AppProps} from 'next/app';
 
-function pics(list){
+
+export default function Gallery() {
+    const [count, currentImage] = useState(0);
+    const hasNext = count < Images.length-1;
+    const hasPrevious = count > Images.length-1;
+
+    const image = Images[count];
+    
+    function handleUpClick() {
+        if(hasNext){
+            currentImage(count+1);
+        } else {
+            currentImage(0)
+        }
+    }
+    function handleDownClick() {
+        if(hasPrevious){
+            currentImage(count-1);
+        } else {
+            currentImage(Images.length-1)
+        }
+    }
+
     return(
-        <div></div>
-    )
+        <>
+            <h1>Image Gallery</h1>
+            <p>
+                <backButton count={count} onClick={handleDownClick}/>
+                <imageDiv image = {image}/>
+                <nextButton count={count} onClick={handleUpClick}/>
+            </p>
+        </>
+    );
 }
 
-export default function image(){
-    return 0
+function nextButton({onClick}: AppProps){
+    return(
+        <button onClick={onClick}>Next</button>
+    );
+}
+
+function backButton({onClick}: AppProps){
+    return(
+        <button onClick={onClick}>Back</button>
+    );
+}
+
+function imageDiv({image}: AppProps){
+    return(
+        <img src={image.link} />
+    );
 }
